35bda169deee466c3dbe368aa1ddbae0743ead69
91630 <91630@LAPTOP-20HITC0F>
2022-12-03 15:20:36 +0530

    this is exp 11

---
+++ src/main/java/Experiment_11/Experiment_11/App.java
@@ -0,0 +1,17 @@
+package Experiment_11.Experiment_11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to devops lab" );
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
